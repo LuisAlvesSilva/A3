f4974009c81b823db14948d9bc47b9500f3dbc9e
Luis Fernando Alves da Silva <121822005+LuisAlvesSilva@users.noreply.github.com>
2024-06-13 08:19:10 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,20 @@
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
     const questions = document.querySelectorAll(".question");
     const result = document.getElementById("result");
     const congratsImage = document.getElementById("congrats-image");
+    const overlay = document.querySelector(".overlay");
     let currentQuestionIndex = 0;
 
     function showNextQuestion() {
         questions[currentQuestionIndex].classList.remove("active");
         congratsImage.style.display = "none";
+        overlay.style.display = "none";
         currentQuestionIndex++;
         if (currentQuestionIndex < questions.length) {
             questions[currentQuestionIndex].classList.add("active");
         } else {
             result.textContent = "Você completou o jogo!";
+            result.style.color = "blue"; 
         }
     }
 
@@ -23,19 +25,23 @@ document.addEventListener("DOMContentLoaded", () => {
             if (option.getAttribute("data-answer") === "correct") {
                 result.textContent = "Correto! Boa escolha.";
                 result.style.color = "green";
-                congratsImage.style.display = "block";
+                option.style.borderColor = "green"; 
 
+                overlay.style.display = "block"; 
+                congratsImage.style.display = "block"; 
                 setTimeout(() => {
                     result.textContent = "";
                     showNextQuestion();
-                }, 2000); // Mostra a imagem de parabéns por 2 segundos
+                }, 2000);
             } else {
                 result.textContent = "Errado! Tente novamente.";
                 result.style.color = "red";
+                option.style.borderColor = "red"; 
 
                 setTimeout(() => {
                     result.textContent = "";
-                }, 2000); // Mantém a mensagem de erro por 2 segundos
+                    option.style.borderColor = ""; 
+                }, 2000);
             }
         });
     });
